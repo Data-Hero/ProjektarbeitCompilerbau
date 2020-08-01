596f0b13e2201de66dd0ec07c6927958e0479821
riese <b.riesenberg@gmx.de>
2020-08-01 14:57:40 +0200

    main reads file

--- src/main/java/de/fhdo/fsc/project/Main.java
+++ src/main/java/de/fhdo/fsc/project/Main.java
@@ -2,16 +2,22 @@ package de.fhdo.fsc.project;
 
 import date.*;
 
-import java.io.StringReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        StringReader sr = new StringReader("32-02-2001 22:34 pm");
-        DateParserTokenManager scanner = new DateParserTokenManager(new SimpleCharStream(sr));
-        Token t = scanner.getNextToken();
-        while (t.kind != DateParserConstants.EOF) {
-            System.out.println("Found: " + t.image + "(" + t.kind + ")");
-            t = scanner.getNextToken();
+        try(FileReader fr = new FileReader("./src/main/test/test1.csv");BufferedReader br = new BufferedReader(fr)) {
+            NewAwkParserTokenManager scanner = new NewAwkParserTokenManager(new SimpleCharStream(br));
+            Token t = scanner.getNextToken();
+            while (t.kind != NewAwkParserConstants.EOF) {
+                System.out.println("Found: " + t.image + "(" + t.kind + ")");
+                t = scanner.getNextToken();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
