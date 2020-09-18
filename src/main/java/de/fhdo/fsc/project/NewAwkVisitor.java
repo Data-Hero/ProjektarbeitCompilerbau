9a3a2be34590f762da5a4e229c26f3e73a6edb68
riese <b.riesenberg@gmx.de>
2020-09-18 11:12:05 +0200

    AddExpression finished

--- src/main/java/de/fhdo/fsc/project/NewAwkVisitor.java
+++ src/main/java/de/fhdo/fsc/project/NewAwkVisitor.java
@@ -1,11 +1,13 @@
 package de.fhdo.fsc.project;
 
 import de.fhdo.fsc.project.DataType.ArrayType;
+import de.fhdo.fsc.project.DataType.TypeException;
 import de.fhdo.fsc.project.DataType.TypeI;
-import de.fhdo.fsc.project.DataType.Types;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class NewAwkVisitor implements NewAwkParserVisitor {
 
@@ -61,19 +63,49 @@ public class NewAwkVisitor implements NewAwkParserVisitor {
     }
 
     @Override
-    public Object visit(ASTAdditiveExpression node, Object data) {
+    public Object visit(ASTAdditiveExpression node, Object data) throws TypeException {
         node.childrenAccept(this, data);
         Object a = pop();
         Object b = pop();
-        if(symbolTable.get(a).isNumericType() && symbolTable.get(b).isNumericType()) {// Integer, Double, Character, Boolean, String, Array (Konkatenation und Entfernen von gleichen Elementen)
-            Double e = ((Double) a) + ((Double) b);
-            stack.addFirst(e);
-        } else if ( symbolTable.get(a).isNumericType() && !symbolTable.get(b).isArrayType()
-                || !symbolTable.get(a).isArrayType() && symbolTable.get(b).isNumericType()) {
+        TypeI aType = symbolTable.get(a);
+        TypeI bType = symbolTable.get(b);
+        /**
+         * Cases (commutative):
+         * + Numeric + Numeric (add)
+         * + Numeric + Expression (String concatenate)
+         * + Numeric + Array (Array concatenate)
+         * + Array + Array (Array concatenate)
+         * - Expression + Expression (String concat)
+         * - Expression + Array (concat if baseTyp = Expression, otherwise TypeError)
+         */
+        if(aType.isNumericType() && symbolTable.get(b).isNumericType()) {// Integer, Double, Character, Boolean, String, Array (Konkatenation und Entfernen von gleichen Elementen)
+            Double e = ((Double) a) + ((Double) b); //TODO Save only int or char if its only int or char
+            stack.addFirst(e);                      //TODO do i have to save e into my symbolTable?
+        } else if (aType.isNumericType() && !bType.isArrayType()
+                || !aType.isArrayType() && bType.isNumericType()) {     // Numeric Type and non-array, non-numeric type
             String e = String.valueOf(a) + String.valueOf(b);
             stack.addFirst(e);
-        } else if (symbolTable.get(a).isNumericType() && symbolTable.get(b).isArrayType()) {
-                // Check baseType
+        } else if (aType.isNumericType() && bType.isArrayType()) {      // Numeric Type and array type (prepend a to b)
+            arrayAddition(a,b);
+        } else if (aType.isArrayType() && bType.isNumericType()) {
+            arrayAddition(b,a);
+        } else if (aType.isArrayType() && bType.isArrayType()) {
+            TypeI aBaseType = ((ArrayType)aType).getBaseTyp();
+            TypeI bBaseType = ((ArrayType)bType).getBaseTyp();
+            if(aBaseType.equals(bBaseType)) {
+                List<Object> newList = new ArrayList<>();
+                newList.addAll((List<Object>)a);
+                newList.addAll((List<Object>)b);
+            } else {
+                throw new TypeException();
+            }
+        } else if (isExpression(aType) && isExpression(bType)) {
+            String e = String.valueOf(a) + String.valueOf(b);
+            stack.addFirst(e);
+        } else if (bType.isArrayType()){ // Expression + Array (concat if baseTyp = Expression, otherwise TypeError)
+            arrayAddition(a,b);
+        } else {
+            arrayAddition(b,a);
         }
         return null;
     }
@@ -203,6 +235,24 @@ public class NewAwkVisitor implements NewAwkParserVisitor {
 
     private Object pop() {
         return stack.removeFirst();
+    }
+
+    private void arrayAddition(Object a, Object b) throws TypeException {
+        TypeI aType = symbolTable.get(a);
+        TypeI bType = symbolTable.get(b);
+        ArrayType oldArrayType = (ArrayType)bType;
+        if (aType.equals(oldArrayType.getBaseTyp())) {
+            ArrayType newArray = new ArrayType(oldArrayType.getBaseTyp());
+            List<Object> array = new ArrayList<>(); // TODO use String instead of Object?
+            array.add(a);
+            array.addAll((List<Object>) b);
+            stack.addFirst(array);
+        } else
+            throw new TypeException();
+    }
+
+    private boolean isExpression(TypeI t) {
+        return !t.isArrayType() && !t.isNumericType();
     }
 
 }
