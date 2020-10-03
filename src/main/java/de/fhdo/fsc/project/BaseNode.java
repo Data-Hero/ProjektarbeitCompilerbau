07eacfa0bde178edbecb39b07f6705a1928e65b4
Alexander Weidemann <walnuss0815@gmail.com>
2020-10-03 14:12:09 +0200

    Scanner and parser

--- src/main/java/de/fhdo/fsc/project/BaseNode.java
+++
@@ -1,7 +0,0 @@
-package de.fhdo.fsc.project;
-
-import java.util.HashMap;
-
-public class BaseNode {
-    public HashMap data = new HashMap();
-}
--- src/main/java/de/fhdo/fsc/project/Main.java
+++ src/main/java/de/fhdo/fsc/project/Main.java
@@ -1,41 +1,22 @@
 package de.fhdo.fsc.project;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try(FileReader fr = new FileReader("./src/main/test/test1.na");BufferedReader br = new BufferedReader(fr)) {
-            NewAwkParser parser = new NewAwkParser(new FileReader("./src/main/test/test1.na"));
-            //SimpleNode node = parser.Start();
-            ASTStart node = parser.Start();
-            NewAwkParserVisitor visitor = new NewAwkVisitor();
-
-            NewAwkParserTokenManager scanner = new NewAwkParserTokenManager(new SimpleCharStream(br));
-            Token t = scanner.getNextToken();
-            while (t.kind != NewAwkParserConstants.EOF) {
-                System.out.println("Found: " + t.image + " (" + t.kind + ")");
-                t = scanner.getNextToken();
+        try {
+            NewAwkParser parser = new NewAwkParser(new BufferedInputStream(
+                    new FileInputStream("./src/main/test/test1.na")
+            ));
+            try {
+                parser.compilationUnit();
+            } catch (de.fhdo.fsc.project.ParseException e) {
+                e.printStackTrace();
             }
-            traverseNodes(node, visitor);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void traverseNodes(SimpleNode node, NewAwkParserVisitor visitor) {
-        for(int i=0;i<node.jjtGetNumChildren();i++) {
-            //System.out.println(node.value);
-            //System.out.println(node.getClass().getSimpleName());
-            SimpleNode simpleNode = (SimpleNode)node.jjtGetChild(i);
-            simpleNode.jjtAccept(visitor, null);
-            traverseNodes(simpleNode, visitor);
+        } catch (IOException ex) {
+            System.out.println(ex);
         }
     }
 }
