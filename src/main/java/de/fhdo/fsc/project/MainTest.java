42cde80eba8d88a9ca194a0e4ee433734ee4eabf
riese <b.riesenberg@gmx.de>
2020-10-08 18:35:04 +0200

    MainTest updated

--- src/main/java/de/fhdo/fsc/project/MainTest.java
+++ src/main/java/de/fhdo/fsc/project/MainTest.java
@@ -8,15 +8,24 @@ public class MainTest {
 
     public static void main(String[] args) {
         String[] arguments = new String[1];
-
+        StringBuilder sb = new StringBuilder();
         try {
             arguments[0] = "./src/main/test/testMultiDimArray.na";
             Main.main(arguments);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("testMultiDimArray passed? " +
-                testOutput("./src/main/test/out.txt", "[\"a\", \"c\"]"));
+        sb.append("testMultiDimArray passed? ").
+                append(testOutput("./src/main/test/out.txt", "[\"a\", \"c\"]")).append("\n");
+
+        try {
+            arguments[0] = "./src/main/test/testForeach.na";
+            Main.main(arguments);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sb.append("testForeach passed? ").
+                append( testOutput("./src/main/test/out2.txt", "42")).append("\n");
 
         try {
             arguments[0] = "./src/main/test/testArrayIndex.na";
@@ -24,9 +33,10 @@ public class MainTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("testArrayIndex passed? " +
-                testOutput("./src/main/test/out2.txt", "42"));
+        sb.append("testArrayIndex passed? ").
+                append( testOutput("./src/main/test/out3.txt", "45")).append("\n");
 
+        System.out.println(sb.toString());
     }
 
     private static boolean testOutput(String pathToFile, String expectedValue) {
